5b8edb92bbde4e6d4d2e6940f54637322d745930
Pauline <huby.pauline@gmail.com>
2022-08-30 16:00:20 -0400

    first commit

---
+++ paddle_cls.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+# to inherit from the Turtle class, pass it in parameter 
+# also need super() function to give access to methods and properties of Turtle
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
